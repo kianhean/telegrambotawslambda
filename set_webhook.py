7778299d3c1942a9487e98ed97874a50fbc6cec8
dfpoon <dfpoon@users.noreply.github.com>
2018-03-11 09:28:16 +0800

    set webhook by a python script

---
+++ set_webhook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+bot_token = <your telegram token here>
+
+import telepot
+bot = telepot.Bot(bot_token)
+
+#Set AWS endpoint gateway
+bot.setWebhook("https://XXXXXXXXX.execute-api.us-east-1.amazonaws.com/dev")
+
+#terminate webhook if sth gets wrong
+#bot.setWebhook("")
+
+print(bot.getMe())
